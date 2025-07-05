34989f5d6d7fb7837d245726d351a725ad458646
amandalyckenius <alyckenius@gmail.com>
2025-07-06 01:43:37 +0200

    chore: delete boilerplate files and unused imports

--- src/components/CapsuleForm.tsx
+++ src/components/CapsuleForm.tsx
@@ -1,8 +1,26 @@
 const CapsuleForm = () => {
     return(
-        <div>
+        <form action="">
 
-        </div>
+            <label>
+                Email:
+                <input type="email" name="email" required />
+            </label>
+
+            <label>
+                Delivery date:
+                <input type="date" name="deliveryDate" required />
+            </label>
+
+            <label>
+                Message:
+                <textarea placeholder="Write your vision for your future here"></textarea>
+            </label>
+
+            <button type="submit">Send</button>
+
+
+        </form>
     )
 }
 
