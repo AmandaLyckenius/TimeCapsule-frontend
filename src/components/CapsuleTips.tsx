f5ae9af156b9bbf7a442469a84dc63adeb7578ce
amandalyckenius <alyckenius@gmail.com>
2025-07-06 02:41:18 +0200

    add: CapsuleTips component

---
+++ src/components/CapsuleTips.tsx
@@ -0,0 +1,18 @@
+const CapsuleTips = () => {
+    return (
+        <div>
+            <h2>Here are some questions to get you inspired</h2>
+            <ul>
+                <li>Where do you live?</li>
+                <li>How do you spend a typical day?</li>
+                <li>Have you traveled?</li>
+                <li>Have you met someone famous?</li>
+                <li>Have you learnt a new skill?</li>
+                <li>What kind of music do you listen to?</li>
+                <li>more examples here</li>
+            </ul>
+        </div>
+    )
+}
+
+export default CapsuleTips;
