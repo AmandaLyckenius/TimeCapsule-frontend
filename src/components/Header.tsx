8f0518aaa9c9f794f04aaf203b2b3ae43c340e07
amandalyckenius <alyckenius@gmail.com>
2025-08-06 11:57:33 +0200

    Add: css to header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
     return(
         <header>
             <h1>Vision Capsule</h1>
-            <p>Your time capsule </p>
+            <p>Vision your future </p>
         </header>
     )
 }
