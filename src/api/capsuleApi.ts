024bd46ce176fe5fc2dde0e8c9ac11d27f8d4b7f
amandalyckenius <alyckenius@gmail.com>
2025-08-14 01:10:58 +0200

    Add: base url to env local and function to assert base url in api

--- src/api/capsuleApi.ts
+++ src/api/capsuleApi.ts
@@ -1,11 +1,21 @@
 
-const BASE_URL = 'https://time-capsule.fly.dev';
+const BASE_URL = process.env.REACT_APP_API_URL;
+
+function assertBaseUrl(url?: string): asserts url is string {
+    if (!url) {
+      throw new Error(
+        'Saknar REACT_APP_API_URL. Sätt den i Vercel > Project > Settings > Environment Variables (Production/Preview) eller i .env.local för dev.'
+      );
+    }
+  }
 
 export const sendCapsule = async (capsuleData: {
     email: string;
     message: string;
     deliveryDate: string;
 }) => {
+
+    assertBaseUrl(BASE_URL);
 
     const response = await fetch (`${BASE_URL}/api/time-capsule`, {
         method: 'POST',
